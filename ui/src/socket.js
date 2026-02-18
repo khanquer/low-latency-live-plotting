b5251fe409e74b1abf6f13c69edb9664eeeaebb4
Imrhankhan Shajahan <imrhankhan.shajahan@modelon.com>
2026-02-18 19:23:45 +0530

    fix: update url to be dynamic

--- ui/src/socket.js
+++ ui/src/socket.js
@@ -2,7 +2,8 @@ import { io } from 'socket.io-client';
 
 // In a Docker Compose setup, 'localhost:5000' works because the 
 // browser (on your host machine) maps to the container's exposed port.
-const URL = "http://localhost:8000";
+const SOCKET_PORT = 8000; 
+const URL = `${window.location.protocol}//${window.location.hostname}:${SOCKET_PORT}`;
 
 export const socket = io(URL, {
     autoConnect: false, // We manually connect when the app loads
